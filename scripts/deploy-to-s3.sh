43b7fce2886216b67de4848aee66afdc4c5bd740
Tom Horton <thomasphorton@gmail.com>
2017-10-10 20:36:06 -0700

    Adds parameters to upload script

--- scripts/deploy-to-s3.sh
+++ scripts/deploy-to-s3.sh
@@ -1,2 +1,23 @@
-aws s3 sync ./src s3://thomasphorton.com --profile thomasphorton --delete
+#! /bin/sh
 
+PROFILE="default"
+BUCKET="thomasphorton.com"
+
+# Get command line parameters
+while [ "$1" != "" ]; do
+  case $1 in
+    -p | --profile )
+    shift
+    PROFILE=$1
+    ;;
+
+    -b | --bucket-name )
+    shift
+    BUCKET=$1
+    ;;
+
+  esac
+  shift
+done
+
+aws s3 sync ./src s3://$BUCKET --profile $PROFILE --delete
