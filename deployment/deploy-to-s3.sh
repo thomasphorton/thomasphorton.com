3b2e76f2eb64b4d31e492d9e4d31ac4e2254f93a
Tom Horton <thomasphorton@gmail.com>
2018-01-23 21:09:39 -0800

    Switches to hexo build

---
+++ deployment/deploy-to-s3.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+BUCKET="thomasphorton.com"
+
+# Get command line parameters
+while [ "$1" != "" ]; do
+  case $1 in
+    -p | --profile )
+    shift
+    PROFILE=$1
+    ;;
+
+    -b | --bucket-name )
+    shift
+    BUCKET=$1
+    ;;
+
+  esac
+  shift
+done
+
+aws s3 sync ./blog/public s3://$BUCKET
